d69f1eaecb7d0c5e571196dd90cc1b57c70a6eff
szalatnyaibalazs <szalatnyaibalazs@gamil.com>
2020-09-16 08:28:58 +0200

    Add project files.

---
+++ Kiralynok/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kiralynok
+{
+    class Tabla
+    {
+        char[,] T = new char[8,8];
+        char UresCella;
+        int UresOszlopokSzama;
+        int UresSorokSzama;
+        public void Elhelyez()
+        {
+
+        }
+
+        public void FajlbaIr()
+        {
+
+        }
+
+        public void Megjelenit()
+        {
+
+        }
+
+        public Tabla(char r)
+        {
+
+        }
+        public int UresOszlop()
+        {
+
+            return 0;
+        }
+
+        public int UresSor()
+        {
+
+            return 0;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
